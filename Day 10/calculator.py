bb8099b84490bb199e72e5ddf6313c7d37e0ba52
pallavirana65 <narain.pallavi@gmail.com>
2023-02-21 14:44:37 -0600

    Add Day 10 project

---
+++ Day 10/calculator.py
@@ -0,0 +1,52 @@
+# Day 10 Project - Calculator
+# Choose a first number, operation, and second number. This code will calculate the operation between the two numbers and print the answer. Then the user can choose to continue calculations using the answer or start over with a new calculation.
+
+from art import logo
+
+# Add
+def add(n1, n2):
+  return n1 + n2
+
+# Subtract
+def subtract(n1, n2):
+  return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+  return n1 * n2
+
+# Divide
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  '+' : add, 
+  '-' : subtract, 
+  '*' : multiply, 
+  '/' : divide
+}
+
+def calculator():
+  print(logo)
+  
+  num1 = float(input('What is the first number? '))
+  for symbol in operations: 
+    print(symbol)
+  should_continue = True
+  
+  while should_continue:
+    operation_symbol = input('Pick an operation: ')
+    num2 = float(input('What is the next number? '))
+    
+    answer = operations[operation_symbol](num1, num2)
+    
+    print(f'{num1} {operation_symbol} {num2} = {answer}')
+    
+    if input(f'Type "y" to continue calculating with {answer}, or type "n" to start a new calculation.: ') == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+      # Recursion - resets back to the beginning
+
+calculator()
